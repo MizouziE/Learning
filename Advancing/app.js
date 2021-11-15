d0a866c015499215fe006221cb92540284a94232
MizouziE <90829439+MizouziE@users.noreply.github.com>
2021-11-15 12:02:12 +0100

    filled in the bits that were missed

--- Advancing/app.js
+++ Advancing/app.js
@@ -9,3 +9,31 @@ anchorElement.href = 'https://google.com';
 //This selects like a css selector
 // anchorElement = document.querySelector('a');
 // anchorElement.href = 'https://google.com';
+
+//Add a new element
+//1. Create it
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://laracasts.com';
+newAnchorElement.textContent = 'This leads to Laracasts for when you\'re sick of JavaScript'
+
+//2. Access the parent
+
+let = firstParagraph = document.querySelector('p');
+
+//3. Insert it
+
+firstParagraph.append(newAnchorElement);
+
+// Remove elements
+//1. Select it
+
+let firstH1Element = document.querySelector('h1');
+
+//2. Remove it
+
+firstH1Element.remove();
+// for older browsers use this
+// firstH1Element.parentElement.removeChild(firstH1Element);
+
+//Move elements
