742c6ed848b2e0032e306c9db49c170c04e988c9
MizouziE <90829439+MizouziE@users.noreply.github.com>
2021-11-15 11:30:54 +0100

    DOM drilling and element query selection

--- Advancing/app.js
+++ Advancing/app.js
@@ -1,4 +1,11 @@
-console.dir(document);
+// document.body.children[1].children[0].href = 'https://google.com';
 
 // alert('piss...');
 // window.alert('...off');
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com';
+
+//This selects like a css selector
+// anchorElement = document.querySelector('a');
+// anchorElement.href = 'https://google.com';
