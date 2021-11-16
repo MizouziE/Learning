e38e3036fa9205aaa92f1f6d4b1ae0154dd48849
MizouziE <90829439+MizouziE@users.noreply.github.com>
2021-11-16 14:24:33 +0100

    three down one to go (loops in action)

--- Advancing/loops-in-action/loops-in-action.js
+++ Advancing/loops-in-action/loops-in-action.js
@@ -19,3 +19,46 @@ function calculateSum() {
 }
 
 calculateSumButtonElement.addEventListener('click', calculateSum)
+
+// Highlight links
+
+const highlightLinksButtonElement = document.querySelector('#highlight-links button');
+
+function highlightLinks() {
+    const anchorElements = document.querySelectorAll('#highlight-links a');
+
+    for (const anchorElement of anchorElements) {
+        anchorElement.classList.add('highlight');
+    }
+}
+
+highlightLinksButtonElement.addEventListener('click', highlightLinks);
+
+// Display User Data
+
+const dummyUserData = {
+    firstName: 'Sam',
+    lastName: 'Anglin',
+    age: 34,
+    height: '180cm'
+};
+
+const displayUserDataButtonElement = document.querySelector('#user-data button');
+
+function displayUserData() {
+    const outputDataElement = document.getElementById('output-user-data');
+
+    outputDataElement.innerHTML = '';
+
+    for (const key in dummyUserData) {
+        const newUserDataListItemElement = document.createElement('li');
+        const outputText = key.toUpperCase() + ': ' + dummyUserData[key]
+
+        newUserDataListItemElement.textContent = outputText;
+        outputDataElement.append(newUserDataListItemElement);
+    }
+}
+
+displayUserDataButtonElement.addEventListener('click', displayUserData)
+
+// Statistics
