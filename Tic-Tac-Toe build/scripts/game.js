d7a7b8661670b97334b0944666c6c126ea09bd75
MizouziE <90829439+MizouziE@users.noreply.github.com>
2021-11-23 13:09:52 +0100

    game field logic

--- Tic-Tac-Toe build/scripts/game.js
+++ Tic-Tac-Toe build/scripts/game.js
@@ -4,5 +4,34 @@ function startNewGame() {
         return;
     }
 
+    activePlayerNameElement.textContent = players[activePlayer].name;
     gameAreaElement.style.display = 'block';
+}
+
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+
+    const selectedField = event.target;
+    const selectedColumn = selectedField.dataset.col - 1;
+    const selectedRow = selectedField.dataset.row - 1;
+
+    if (gameData[selectedRow][selectedColumn] > 0) {
+        alert('That\'s already taken!');
+        return;
+    }
+
+    selectedField.textContent = players[activePlayer].symbol;
+    selectedField.classList.add('disabled');
+
+    gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+    switchPlayer();
 }
