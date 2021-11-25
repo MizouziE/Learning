a4e71c5a903c7f5cfe34a10496babde1e2e5e89f
MizouziE <90829439+MizouziE@users.noreply.github.com>
2021-11-25 15:08:19 +0100

    storing input data

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,18 +1,21 @@
-const http = require('http');
+const express = require('express');
 
-function handleRequest(request, response) {
-    
-    if (request.url === '/currenttime') {
-        response.statusCode = 200;
-        response.end('<h1>' + new Date().toISOString() + '</h1>');
-    } else if (request.url === '/') {
-        response.statusCode = 200;
-        response.end('<h1>Whagwaan?</h1>');
-    }
+const app = express();
 
-}
+app.use(express.urlencoded({extended: false}));
 
-const server = http.createServer(handleRequest);
+app.get('/currenttime', function(req, res) {
+    res.send('<h1>' + new Date().toISOString() + '</h1>')
+});
 
-server.listen(3000);
+app.get('/', function(req, res) {
+    res.send('<form action="/store-user" method="POST"><label>What\'s your name?</label><input type="text" name="username"><button>Save</button></form>')
+});
 
+app.post('/store-user', function(req, res) {
+    const userName = req.body.username;
+    console.log(userName);
+    res.send('<h1>Now we have your name!</h1>');
+});
+
+app.listen(3000);
